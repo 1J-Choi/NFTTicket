b209e6ad5ae18ba82e1894d94abcc7770c46f8fa
1J-Choi <chldnjswp110@naver.com>
2024-04-01 17:37:34 +0900

    Initial commit

---
+++ src/main/java/NFTTicket/NftTicketApplication.java
@@ -0,0 +1,13 @@
+package NFTTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftTicketApplication.class, args);
+	}
+
+}
