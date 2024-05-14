4ab66abf0aa5cf014273a51fb2cf599dcfbd3559
Eunsol <qeqe147258@gmail.com>
2024-05-14 18:38:43 +0900

    EventSearchDto 작성

---
+++ src/main/java/NFTTicket/dto/EventSearchDto.java
@@ -0,0 +1,13 @@
+package NFTTicket.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EventSearchDto {
+
+    private String searchBy;
+
+    private String searchQurey = "";
+}
