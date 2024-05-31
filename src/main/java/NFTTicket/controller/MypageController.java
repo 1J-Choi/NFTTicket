3147bdb96ba1af6e50117d10d947a195dc367e13
sunwoo <32180217@dankook.ac.kr>
2024-05-31 18:30:25 +0900

    mypage 컨트롤러 수정 및 mypage 클래스 이름 수정 헤더 마이페이지 링크 수정

--- src/main/java/NFTTicket/controller/MypageController.java
+++ src/main/java/NFTTicket/controller/MypageController.java
@@ -1,13 +1,18 @@
 package NFTTicket.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+@Controller
+@RequiredArgsConstructor
 public class MypageController {
 
     @GetMapping(value = "/mypage")
     public String mypageUser(){
-        return "/mypage/mypage";
+        return "mypage/mypage";
     }
 
 }
