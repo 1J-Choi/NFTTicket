f5712c16f58b6c28436f25c0e813c1f543fa48cd
1J-Choi <chldnjswp110@naver.com>
2024-06-05 19:16:24 +0900

    TicketRepositoryCustomImpl JOIN절 수정 시도

--- src/main/java/NFTTicket/repository/TicketRepositoryCustomImpl.java
+++ src/main/java/NFTTicket/repository/TicketRepositoryCustomImpl.java
@@ -48,7 +48,8 @@ public class TicketRepositoryCustomImpl implements TicketRepositoryCustom{
 
         QueryResults<TicketShowDto> results = queryFactory.select(new QTicketShowDto(ticket.id, event.evName, event.date,
                         event.place, event.member.nick, eventImg.imgURL))
-                .from(ticket).join(ticket.event, event)
+                .from(ticket).leftJoin(event).on(ticket.event.id.eq(event.id))
+                .leftJoin(eventImg).on(eventImg.event.id.eq(event.id))
                 .where(ticketEq(ticketBoxId), searchByLike(ticketSearchDto.getSearchBy(), ticketSearchDto.getSearchQuery()))
                 .limit(pageable.getPageSize()).fetchResults();
 
